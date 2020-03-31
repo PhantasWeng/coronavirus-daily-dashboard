beb6c7b9942060e42ca3735b950a74fb4e9bfdd0
Phantas Weng <bensonuong@gmail.com>
2020-03-31 18:34:47 +0800

    modified: RWD

--- src/components/OwltingNav.vue
+++ src/components/OwltingNav.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="owltingNav">
     <div class="flex items-center flex-1">
-      <img :src="logo" alt="owlting-logo" width="24" class="py-4 mr-8">
-      <a href="http://www.owlting.com/" target="_blank" class="no-underline font-bold uppercase"><span class="text-blue-light">奧丁丁</span> <span class="text-red">Owlting</span></a>
+      <img :src="logo" alt="owlting-logo" width="32" class="hidden md:block md:py-4 md:mr-8">
+      <div>
+        <div class="text-xs text-grey font-bold mb-4">Sponsor</div>
+        <a href="http://www.owlting.com/" target="_blank" class="no-underline font-bold uppercase"><span class="text-blue-light">奧丁丁</span> <span class="text-red">Owlting</span></a>
+      </div>
     </div>
     <div class="flex items-center flex-wrap justify-end">
       <template v-for="(link, index) in owltingLink">
